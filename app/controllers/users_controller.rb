ae498d76f5460fc80c3df5ae214971b7fe20ba6d
Liz Vo <liz_quack@hotmail.com>
2020-11-17 11:18:45 +0900

    created users model

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+	before_action :set_user, only: [:show, :edit, :update]
+
+	def index
+		@users = User.all
+	end
+
+	def show
+	end
+
+	def edit
+	end
+
+	def update
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
+  end
+
+	private
+	def user_params
+    params.require(:user).permit(:email, :password, :first_name, :last_name, :nickname, :address, :latitude, :longitude, :description, :birthday, :gender)
+  end
+
+	def set_user
+    @user = Vinyl.find(params[:id])
+  end
+end
