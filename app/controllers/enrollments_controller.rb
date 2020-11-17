e59076ae82ecf8d9d02c0bd0379c0b5249d36ddd
Yuping Kuei <kueiyuping@gmail.com>
2020-11-17 13:20:28 +0900

    Completed enrollment controller

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -1,2 +1,44 @@
 class EnrollmentsController < ApplicationController
+  before_action :set_enrollment, only: [:update, :destroy]
+  def new
+    @enrollment = Enrollment.new
+    @event = Event.find(params[event_id])
+    authorize @enrollment
+  end
+
+  def create
+    @enrollment = enrollment.new(enrollment_params)
+    @event = Event.find(params[:event_id])
+    @enrollment.user = current_user
+    @enrollment.event = @event
+    authorize @enrollment
+    if @enrollment.save
+      redirect_to user_path
+    else
+      render "events/show"
+    end
+  end
+
+  def update
+    @enrollment = Enrollment.find(params[:id])
+    authorize @enrollment
+    if @enrollment.update(enrollment_params)
+      redirect_to user_path
+    end
+  end
+
+  def destroy
+    @enrollment = enrollment.find(params[:id])
+    authorize @enrollment
+    @enrollment.destroy
+    redirect_to user_path
+  end
+
+  private
+
+  def set_enrollment
+    params.require(:enrollment).permit(:user_id, :event_id, :start_time, :end_time, :status)
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-	resources :users, only: [:new, :create, :read, :edit, :update]
+  resources :users, only: [:new, :create, :read, :edit, :update]
+  resources :event do
+    resources :enrollments, only: [:new, :create]
+  end
+  resources :enrollments, only: [:update, :destroy]
 end
