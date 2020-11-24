6621183a395af9fdb2b6933ab3a7a896ec4ba772
Liz Vo <liz_quack@hotmail.com>
2020-11-24 14:56:58 +0900

    updated event page styling, form, and removed search from navbar

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -4,10 +4,10 @@ class Enrollment < ApplicationRecord
 
   belongs_to :event
 
-  validates :status, presence: true
-  validates :start_time, presence: true
-  validates :end_time, presence: true
-  validate :end_time_after_start_time
+  # validates :status, presence: true
+  # validates :start_time, presence: true
+  # validates :end_time, presence: true
+  # validate :end_time_after_start_time
 
   def end_time_after_start_time
     return if end_time.blank? || start_time.blank?
